a8ac275cebe0ff5255deacff791468fdc1af2da1
CoderTX1 <coder1.travelx@gmail.com>
2025-07-07 04:54:21 -0700

    solve some error

--- app/Models/ServiceType.php
+++ app/Models/ServiceType.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ServiceType extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name','agency_id'];
 
     public function sales()
     {
